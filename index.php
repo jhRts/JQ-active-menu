cf34fe5d97fefc340cdfbea229ed091f2678f00e
jhRts <jh@rts.dk>
2013-10-06 20:54:24 +0200

    php update

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <title>JQ active menu</title>
     <script src="/JQ-active-menu/lib/jquery-2.0.3.min.js"></script>
+    <script src="menuActive.js"></script>
+    <style>
+        .active{
+            background-color: rgba(42, 255, 45, 0.44);
+        }
+    </style>
 </head>
 <body>
-    <p>Kun over (xampp) server, mappenav er "JQ-active-menu"</p>
+    <p>Kun over (xampp) server, mappenavn er "JQ-active-menu"</p>
     <nav>
         <a href="/JQ-active-menu/index.html">Forside</a>
         <a href="/JQ-active-menu/underside/underside1.html">Underside</a>
